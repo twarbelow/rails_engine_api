3263f81395af1fd804efca0d46424811709f1f64
twarbelow <taija.warbelow@google.com>
2020-09-02 22:10:40 -0700

    create action in merchant controller

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -6,4 +6,13 @@ class Api::V1::MerchantsController < ApplicationController
   def show
     render json: MerchantSerializer.new(Merchant.find(params[:id]))
   end
+
+  def create
+    render json: MerchantSerializer.new(Merchant.create(merchant_params)), status: :created
+  end
+
+  private
+  def merchant_params
+    params.require('data').require('attributes').permit(:name)
+  end
 end
