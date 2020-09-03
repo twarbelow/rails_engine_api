003b5b7e75467ad0ebc581c389a66dc183313fcc
twarbelow <taija.warbelow@google.com>
2020-09-02 20:59:39 -0700

    add id to serializer

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,4 +1,4 @@
 class ItemSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :description, :unit_price, :merchant_id
+  attributes :id, :name, :description, :unit_price, :merchant_id
 end
