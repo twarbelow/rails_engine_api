0da0c17b899b0e66111e3f07cac0a3d97550aa80
twarbelow <taija.warbelow@google.com>
2020-09-02 22:42:28 -0700

    destroy route for merchant

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :items, only: [:index, :show, :create, :update, :destroy]
-      resources :merchants, only: [:index, :show, :create, :update]
+      resources :merchants, only: [:index, :show, :create, :update, :destroy]
     end
   end
 end
