5b18834ce9f9dae92f51729f5335fbcbe61918bd
twarbelow <taija.warbelow@google.com>
2020-09-02 21:01:15 -0700

    typo in index action

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::MerchantsController < ApplicationController
   def index
-    render json: MerchantsSerializer.new(Merchant.all)
+    render json: MerchantSerializer.new(Merchant.all)
   end
 end
