8c816cbecfa4728ee780f5cfb9540885aac08035
twarbelow <taija.warbelow@google.com>
2020-09-02 19:33:03 -0700

    models pass spec

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,4 +1,8 @@
 class Invoice < ApplicationRecord
   belongs_to :customer
   belongs_to :merchant
+
+  validates_presence_of :customer_id
+  validates_presence_of :merchant_id
+  validates_presence_of :status
 end
--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,4 +1,9 @@
 class InvoiceItem < ApplicationRecord
   belongs_to :item
   belongs_to :invoice
+
+  validates_presence_of :item_id,
+                        :invoice_id,
+                        :quantity,
+                        :unit_price
 end
