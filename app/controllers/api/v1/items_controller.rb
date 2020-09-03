77e74328831e59eb0f09f0f6c8872191ce3fbced
twarbelow <taija.warbelow@google.com>
2020-09-02 22:41:28 -0700

    destroy method for item

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -15,6 +15,10 @@ class Api::V1::ItemsController < ApplicationController
     render json: ItemSerializer.new(Item.update(params[:id], item_params))
   end
 
+  def destroy
+    render json: ItemSerializer.new(Item.destroy(params[:id]))
+  end
+
   private
   def item_params
     params.permit(:name, :description, :unit_price, :merchant_id)
