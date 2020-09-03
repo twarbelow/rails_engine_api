9a511ec5819d7b6597ee5e080eea8afe737b021d
twarbelow <taija.warbelow@google.com>
2020-09-02 22:43:21 -0700

    destroy action for merchants

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -15,6 +15,10 @@ class Api::V1::MerchantsController < ApplicationController
     render json: MerchantSerializer.new(Merchant.update(params[:id], merchant_params))
   end
 
+  def destroy
+    render json: MerchantSerializer.new(Merchant.destroy(params[:id]))
+  end
+
 
   private
   def merchant_params
