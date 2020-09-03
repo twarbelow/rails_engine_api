b5c84372ce3111eafa9a7e073be311a08789d224
twarbelow <taija.warbelow@google.com>
2020-09-02 21:11:59 -0700

    test shape of response

--- spec/requests/items/show_spec.rb
+++ spec/requests/items/show_spec.rb
@@ -6,7 +6,9 @@ RSpec.describe 'Item show', type: :request do
   before {get "/api/v1/items/#{item.id}"}
 
   it 'can get all the items' do
-    expect(JSON.parse(response.body)['data'].size).to eq(1)
+    expect(JSON.parse(response.body)['data']['id'].to_i).to eq(item.id)
+    expect(JSON.parse(response.body)['data']['type']).to eq("item")
+    expect(JSON.parse(response.body)['data']['attributes']['name']).to eq(item.name)
   end
 
   it 'returns status code 200' do
