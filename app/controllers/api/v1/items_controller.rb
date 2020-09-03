e7e284bf1ebc08c81fd68707b4f54ef8ec7e933a
twarbelow <taija.warbelow@google.com>
2020-09-02 22:32:21 -0700

    fix spelling error

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -11,6 +11,10 @@ class Api::V1::ItemsController < ApplicationController
     render json: ItemSerializer.new(Item.create(item_params)), status: :created
   end
 
+  def update
+    render json: ItemSerializer.new(Item.update(params[:id], item_params))
+  end
+
   private
   def item_params
     params.permit(:name, :description, :unit_price, :merchant_id)
